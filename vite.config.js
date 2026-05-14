6223e15a8f060c579ce831a3c42b4fa42f316c47
Cahyanudien Aziz Saputra <47025238+cas8398@users.noreply.github.com>
2026-05-14 14:45:36 +0700

    fix path vite config

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,17 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src/",
+
+  publicDir: "src/assets",
+
   build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+
     assetsDir: "assets",
+
     rollupOptions: {
       input: "src/index.html",
     },
   },
-  publicDir: "src/assets",
 });
